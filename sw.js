2217483c0877638e6243ddaa854246ee2d5ee92f
ccc2511 <criedray@gmail.com>
2026-03-20 20:51:07 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'bsky-post-v1';
+const ASSETS = [
+  '/bluesky-post/',
+  '/bluesky-post/index.html',
+  '/bluesky-post/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
